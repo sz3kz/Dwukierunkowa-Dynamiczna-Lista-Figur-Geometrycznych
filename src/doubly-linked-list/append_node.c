34544a618708b75482e87124ad8d11789f70ce41
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-10 04:43:11 -0600

    Modify append_node to append to current node.

--- src/doubly-linked-list/append_node.c
+++ src/doubly-linked-list/append_node.c
@@ -1,7 +1,6 @@
 #include "../../include/doubly-linked-list.h"
 
 void append_node(Node * current_node, Node * additional){
-	Node * last = find_last_node(current_node);
-	last->next_node = additional;
-	additional->previous_node = last;
+	current_node->next_node = additional;
+	additional->previous_node = current_node;
 }
