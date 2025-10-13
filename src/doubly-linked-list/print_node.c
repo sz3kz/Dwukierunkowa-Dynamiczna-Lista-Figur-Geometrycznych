b7369a4f5b43fd3b6e91e7de8178b5fbf6e752c8
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-13 12:56:16 +0200

    Transform print_node for a more human friendly format.

--- src/doubly-linked-list/print_node.c
+++ src/doubly-linked-list/print_node.c
@@ -2,19 +2,15 @@
 
 void print_node(Node * node, bool is_current){
 	if (is_current){
-		printf(" * [%p]:{prev:%14p, next:%14p, name:%s, area:%lf, perimeter:%lf}\n",
+		printf(" * [%p]:\n\tName:%s, \n\tArea:%lf, \n\tPerimeter:%lf\n",
 			node,
-			node->prev,
-			node->next,
 			node->data.name,
 			node->data.area,
 			node->data.perimeter);
 	}
 	else {
-		printf("   [%p]:{prev:%14p, next:%14p, name:%s, area:%lf, perimeter:%lf}\n",
+		printf("   [%p]:\n\tName:%s, \n\tArea:%lf, \n\tPerimeter:%lf\n",
 			node,
-			node->prev,
-			node->next,
 			node->data.name,
 			node->data.area,
 			node->data.perimeter);
