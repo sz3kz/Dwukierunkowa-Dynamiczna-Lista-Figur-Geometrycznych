7b904b3593fd6e46a6a7cd1227e4f853bd49bc8b
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 12:34:24 +0200

    Add comment documentating the behavior of the function.

--- src/list/find_first_node.c
+++ src/list/find_first_node.c
@@ -1,7 +1,11 @@
 #include "../../include/list.h"
 
 Node * find_first_node(Node * current){
-	// the node need not be the head of the list, if the list is proper
+	/* Recursively follows the current node's previous node,
+	 * until there is no previous node (the current node is 
+	 * first node), returns the current node.
+	 * NOTE: the function does no error checking, expects the 
+	 * list to be properly linked.*/
 	while (current->prev != NULL) current = current->prev;
 	return current;
 }
