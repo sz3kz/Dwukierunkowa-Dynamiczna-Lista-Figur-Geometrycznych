4de6b7e9ab8029576a4cc328791e10a4c5022dce
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 12:37:18 +0200

    Add comment documenting the behavior of the function.

--- src/list/delete_list.c
+++ src/list/delete_list.c
@@ -1,6 +1,9 @@
 #include "../../include/list.h"
 
 void delete_list(Node * current){
+	/* Navigates to the first node via the current node and 
+	 * subsequently deletes every in the list.
+	 * NOTE: when given a null pointer, the function just exits.*/
 	if (current == NULL)
 		return;
 	current = find_first_node(current);
