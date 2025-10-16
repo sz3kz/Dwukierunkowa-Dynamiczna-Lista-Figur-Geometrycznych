532639d1540b9b51c9fc01ae76e582e902e1fe51
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 12:31:10 +0200

    Add comment documenting the behavior of the function.

--- src/list/prepend_node.c
+++ src/list/prepend_node.c
@@ -1,6 +1,9 @@
 #include "../../include/list.h"
 
 void prepend_node(Node * current, Node * new){
+	/* Links a new node with the current node (as the previous). If a 
+	 * node already preceeds the current node, link the node to point 
+	 * to the new node as the next node.*/
 	Node * previous = current->prev;
 	if (previous != NULL) previous->next = new;
 
