480f98284126abb055f503c82792c59cae1b37d6
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 12:27:54 +0200

    Add comment documenting the behavior of the function.

--- src/list/append_node.c
+++ src/list/append_node.c
@@ -1,6 +1,9 @@
 #include "../../include/list.h"
 
 void append_node(Node * current, Node * new){
+	/* Links a new node with the current node (as the next). If a node 
+	 * already succeeds the current node, link the node to point to the 
+	 * new node as the previous node.*/
 	Node * next = current->next;
 	if ( next != NULL) next->prev = new;
 
