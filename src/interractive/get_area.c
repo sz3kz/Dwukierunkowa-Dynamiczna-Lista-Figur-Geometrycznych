b82a2ee9bbf9bd46a167dd6b2f7f72e60b74bba3
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 11:32:59 +0200

    Add comment documenting behavior of get_area().

--- src/interractive/get_area.c
+++ src/interractive/get_area.c
@@ -1,6 +1,8 @@
 #include "../../include/interractive.h"
 
 double get_area(void){
+	/* Prompts the user for an "area" positive double value, returns it.
+	 * Will loop prompting until the value inputted is positive. */
 	double area;
 	while (1){
 		printf("Area");
