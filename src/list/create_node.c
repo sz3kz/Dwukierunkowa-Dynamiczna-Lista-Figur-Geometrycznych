a1df243b64a4c668accb97197f07c1bcbf1e2239
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 12:10:05 +0200

    Add comment documenting the behavior of function.

--- src/list/create_node.c
+++ src/list/create_node.c
@@ -1,6 +1,7 @@
 #include "../../include/list.h"
 
 Node * create_node(Shape * data){
+	/* Allocate memory for the Shape struct, return the pointer to it. */
 	Node * ptr = malloc(sizeof(Node));
 	ptr->prev = NULL;
 	ptr->next = NULL;
