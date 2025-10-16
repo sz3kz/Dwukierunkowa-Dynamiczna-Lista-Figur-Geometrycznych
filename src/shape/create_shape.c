3ea5868d14c28a6efed7d819d29e3d7257c392d7
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 12:47:11 +0200

    Add comment documenting the behavior of the function.

--- src/shape/create_shape.c
+++ src/shape/create_shape.c
@@ -1,6 +1,17 @@
 #include "../../include/shape.h"
 
 Shape * create_shape(double perimeter, double area, char * name){
+	/* Allocates memory and initializes the Shape structure.
+	 * NOTE: the order of arguments is specifically defined as
+	 * such: this function is (originally) being called with
+	 * interractive prompt function returns directly inputted
+	 * as arguments to this function. The arguments are evaluated
+	 * from the last to the first, therefore to have the "name"
+	 * value be prompted to the user first, the "name" argument
+	 * must be the last argument of the function. This makes a lot
+	 * of sense if one is familiar with th order in which CPU registers
+	 * are initialized before a "call" instruction in assembly
+	 * (for e.g. via a reverse engineering hobby)*/
 	Shape * shape = malloc(sizeof(Shape));
 	shape->name = name;
 	shape->area = area;
