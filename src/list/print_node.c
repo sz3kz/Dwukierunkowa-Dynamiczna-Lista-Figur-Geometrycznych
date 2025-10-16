77cfea53526f94aa78564f5ee5995301fbfc3dd4
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 12:22:35 +0200

    Break up printf statements to improve code readability.

--- src/list/print_node.c
+++ src/list/print_node.c
@@ -2,13 +2,19 @@
 
 void print_node(Node * node, bool is_current){
 	if (is_current){
-		printf(" * Node:\n\tName:%s, \n\tArea:%lf, \n\tPerimeter:%lf\n",
+		printf(	" * Shape:\n"
+			"	Name: \"%s\"\n"
+			"	Area: %lf\n"
+			"	Perimeter: %lf\n",
 			node->data->name,
 			node->data->area,
 			node->data->perimeter);
 	}
 	else {
-		printf("   Node:\n\tName:%s, \n\tArea:%lf, \n\tPerimeter:%lf\n",
+		printf(	"   Shape:\n"
+			"	Name: \"%s\"\n"
+			"	Area: %lf\n"
+			"	Perimeter: %lf\n",
 			node->data->name,
 			node->data->area,
 			node->data->perimeter);
