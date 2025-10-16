d0ac99f49815448ea962d03bdc91a608a58c8952
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 12:15:02 +0200

    Refractor comment to better explain the behavior of the function.

--- src/list/delete_node.c
+++ src/list/delete_node.c
@@ -2,8 +2,11 @@
 #include "../../include/shape.h"
 
 void delete_node(Node * current){
-	// Warning: this function deletes the structure to the pointer,
-	// Dereferencing the pointer will cause undefined behavior.
+	/* Frees memory containing the structure the pointer is pointing to.
+	 * Links adjacent nodes to the current node.
+	 * NOTE: After this function is called with a node pointer, the pointer
+	 * should NOT be used without reassignment first. Expect bugs from code
+	 * not adhering to this warning.*/
 	Node * prev = current->prev;
 	Node * next = current->next;
 	if (prev != NULL){
