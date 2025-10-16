77ca7f18bd88bb676613d3712b1e78b08d9cc132
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 11:43:19 +0200

    Add comment documenting behavior of get_name().

--- src/interractive/get_name.c
+++ src/interractive/get_name.c
@@ -1,6 +1,8 @@
 #include "../../include/interractive.h"
 
 char * get_name(void){
+	/* Prompts the user for a string value of length NAME_SIZE.
+	 * Nulls the newline character.*/
 	char * name = malloc(sizeof(char) * NAME_SIZE);
 	printf("Name");
 	printf("%s", INPUT_PROMPT);
