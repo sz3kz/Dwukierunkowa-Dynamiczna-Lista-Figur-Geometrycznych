be1939acd7b219d5796b8182f32de57a8adab4af
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 11:40:48 +0200

    Add comment documenting behavior of show_options_usage().

--- src/interractive/show_options_usage.c
+++ src/interractive/show_options_usage.c
@@ -1,6 +1,8 @@
 #include "../../include/interractive.h"	
 
 void show_options_usage(void){
+	/* Loops through characters of VALID_OPTIONS and outputs it accordingly.
+	 * Can detect errors, won't help diagnosing them at the execution level.*/
 	char option;
 	printf("Options:\n");
 	for (int i = 0; VALID_OPTIONS[i] != 0; ++i){
