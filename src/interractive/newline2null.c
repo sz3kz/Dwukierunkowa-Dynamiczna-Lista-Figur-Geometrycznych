439cb6fbdb6e3508755ab60da0af5f22acc2718b
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 12:06:04 +0200

    Add comment documenting the behavior of the function.

--- src/interractive/newline2null.c
+++ src/interractive/newline2null.c
@@ -1,6 +1,8 @@
 #include "../../include/interractive.h"
 
 void newline2null(char * string){
+	/* Loops through string's characters. If a newline character is found,
+	 * null it and return.*/
 	for (int i = 0; i < NAME_SIZE && string[i] != 0 ; ++i){
 		if (string[i] == '\n'){
 			string[i] = 0;
