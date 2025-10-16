adfe1c0f09fb3ac5dba1e4b4392055d27d92e18e
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-10-16 11:47:41 +0200

    Add comment documenting the behavior of get_perimeter().

--- src/interractive/get_perimeter.c
+++ src/interractive/get_perimeter.c
@@ -1,6 +1,8 @@
 #include "../../include/interractive.h"
 
 double get_perimeter(void){
+	/* Prompts the user for a "perimeter" positive double value, returns it.
+	 * Will loop prompting until the value inputted is positive. */
 	double perimeter;
 	while (1){
 		printf("Perimeter");
